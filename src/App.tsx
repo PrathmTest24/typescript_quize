5866fd287d8f6db0bce841fe3077655df6eaa67c
unknown <upalkarp24@gmail.com>
2024-08-09 10:27:40 +0530

    initial comments

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div>
+        <p>this is react app using Typescript 
+
+        </p>
+        <p> First attempt to test the code </p>
+       </div>
+    </div>
+  );
+}
+
+export default App;
